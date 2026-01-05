0bfe66a551267183feb197c5101e5a23fc545d67
Pranav Menon <pranav.menon@live.com>
2026-01-04 18:58:25 -0800

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Pranav Menon on 2026-01-04.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
